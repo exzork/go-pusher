07ab8ce7ee436f5ffa02f39d300506f71aa3d246
Muhammad Eko <muhammadeko.if@gmail.com>
2023-04-18 22:39:55 +0000

    remove unnecessary set header

--- client.go
+++ client.go
@@ -112,7 +112,6 @@ func (c *Client) Subscribe(channel string) (err error) {
 		e := new(bytes.Buffer)
 		json.NewEncoder(e).Encode(data)
 		req, _ := http.NewRequest(c.authUrl, "POST", e)
-		req.Header.Set("Content-Type", "application/json")
 		resp, err := http.DefaultClient.Do(req)
 		if err != nil {
 			return err
